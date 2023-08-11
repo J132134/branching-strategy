5ecdbc52653029e4c55781af1cab376447ce6dda
Chris Jang <chris.jang@xl8.ai>
2023-08-11 10:07:49 +0900

    Fix: test-release-available.sh

--- scripts/test-release-available.sh
+++ scripts/test-release-available.sh
@@ -2,7 +2,7 @@
 
 git fetch -n origin '+refs/heads/releases/*:refs/remotes/origin/releases/*'
 
-for branch in $(git for-each-ref --format='%(refname:short)' 'refs/heads/releases/*'); do
+for branch in $(git for-each-ref --format='%(refname:short)' 'refs/remotes/releases/*'); do
   base_commit=$(git merge-base main $branch)
   base_commit_time=$(git log -1 --format="%at" $base_commit)
   current_commit_time=$(git log -1 --format="%at" $1)
